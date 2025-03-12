6882bf6168859aff20ef94f0f0e0e6d99b5259ba
Clisman Freitas <clismanfreitas.com@gmail.com>
2025-03-11 23:21:28 -0300

    responsivo inicio

--- src/components/rodape/rodape.jsx
+++ src/components/rodape/rodape.jsx
@@ -10,14 +10,14 @@ const Rodape = () => {
         <footer className="baixo">
             <ul className="lista-contato">
                 <li className="email">
-                    <a href="mailto:clismanfreitas.com@gmail.com"> <img src={Email} alt="" />Clismanfreitas.com@gmail.com</a>
+                    <a href="mailto:clismanfreitas.com@gmail.com"> <img src={Email} alt="Email" />Clismanfreitas.com@gmail.com</a>
                 </li>
                 <li></li>
                 <hr />
                 <li className="links">
-                    <a href="https://www.instagram.com/clismanfreiitas_1?igsh=N3o1aTQwZWh5OHJk" target="_blank"><img src={Instagram}></img></a>
-                    <a href="https://www.linkedin.com/in/clisman-freitas-8b748a2b1/" target="_blank"><img src={Linkedi}></img></a>
-                    <a href="https://github.com/ClismanFreitas" target="_blank"><img src={GitHu}></img></a>
+                    <a href="https://www.instagram.com/clismanfreiitas_1?igsh=N3o1aTQwZWh5OHJk" target="_blank"><img src={Instagram}  alt="Link Instagram"></img></a>
+                    <a href="https://www.linkedin.com/in/clisman-freitas-8b748a2b1/" target="_blank"><img src={Linkedi} alt="Link Linkedin"></img></a>
+                    <a href="https://github.com/ClismanFreitas" target="_blank"><img src={GitHu} alt="Link GitHub"></img></a>
                 </li>
                 <p className="creditos">Clisman Freitas</p>
             </ul>
