d8f710276afc54228853fc8f8d0775085e21ae14
Clisman Freitas <clismanfreitas.com@gmail.com>
2025-05-12 13:54:17 -0300

    animaçao

--- src/components/inicio/inicio.jsx
+++ src/components/inicio/inicio.jsx
@@ -13,7 +13,7 @@ const Inicio = () => {
                 <p>Olá! Meu nome é</p>
                 <h1 className='nome-Autor'>Clisman Freitas</h1>
                 <div className="txt-animado">
-                    Develop Front-end
+                    Developer Front-end
                 </div>
                 <div className='icons'>
                     <a href="https://www.linkedin.com/in/clisman-freitas-8b748a2b1/" target="_blank" rel="noopener noreferrer" ><img src={LinkedinIcon} alt="" /></a>
